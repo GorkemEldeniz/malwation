c43a48fa76173b68618f8c29b1005c22282a7807
Görkem Eldeniz <82877261+GorkemEldeniz@users.noreply.github.com>
2023-08-27 18:16:05 +0300

    button componenti güncellendi

---
+++ client/src/components/ui/button.tsx
@@ -0,0 +1,107 @@
+import { ButtonHTMLAttributes } from "react";
+
+import { Icon } from "@icon";
+
+import { cva, VariantProps } from "class-variance-authority";
+import { cn } from "@utils/index";
+
+const button = cva("rounded-md relative", {
+  variants: {
+    variant: {
+      default: "bg-white text-text shadow-button border border-gray-400",
+      primary: "bg-primary text-white",
+      destructive: "bg-destructive text-white",
+      monochrome: "bg-white text-text border border-gray-400",
+    },
+    modifier: {
+      outline:
+        "bg-transparent border border-current shadow-[0_0_0_1px_currentColor]",
+      plain: "shadow-none bg-transparent border-none px-2 py-1 text-sm",
+    },
+    size: {
+      slim: "text-sm",
+      medium: "text-sm",
+      large: "text-base",
+    },
+    fullWidth: {
+      true: "w-full",
+    },
+    disabled: {
+      true: "opacity-70",
+    },
+  },
+  compoundVariants: [
+    {
+      modifier: "outline",
+      variant: "destructive",
+      className: "text-destructive",
+    },
+    {
+      modifier: "outline",
+      variant: "primary",
+      className: "text-primary",
+    },
+    {
+      modifier: "plain",
+      variant: "destructive",
+      className: "text-destructive",
+    },
+    {
+      modifier: "plain",
+      variant: "primary",
+      className: "text-primary",
+    },
+  ],
+  defaultVariants: {
+    variant: "default",
+    size: "medium",
+  },
+});
+
+interface ButtonProps
+  extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, "disabled">,
+    VariantProps<typeof button> {
+  loading?: boolean;
+  label: string;
+}
+
+function Button({
+  variant,
+  modifier,
+  size,
+  fullWidth,
+  disabled,
+  label,
+  loading = false,
+  className,
+  ...rest
+}: ButtonProps) {
+  return (
+    <button
+      {...rest}
+      disabled={!!disabled}
+      className={cn(
+        button({ variant, modifier, size, fullWidth, disabled }),
+        className
+      )}
+    >
+      <span className={cn("leading-[1.25rem]", loading && "text-transparent")}>
+        {label}
+      </span>
+      {loading && (
+        <span className="absolute left-1/2 top-1/2 block h-4 w-4 -translate-x-1/2 -translate-y-1/2">
+          <Icon
+            width="20"
+            height="20"
+            className="animate-spin"
+            fill="currentColor"
+            icon="spinner"
+          />
+          <span className="sr-only">Loading</span>
+        </span>
+      )}
+    </button>
+  );
+}
+
+export default Button;
