8544d26eafd3845b4ab20fd30305ab0b9d851205
Görkem Eldeniz <82877261+GorkemEldeniz@users.noreply.github.com>
2023-08-28 10:16:51 +0300

    eslint

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -4,30 +4,6 @@ module.exports = {
       files: ["*.ts", "*.tsx", "*.js"],
       parser: "@typescript-eslint/parser",
     },
-    {
-      files: ["*.js", "*.jsx", "*.ts", "*.tsx"],
-      rules: {
-        "simple-import-sort/imports": [
-          "error",
-          {
-            groups: [
-              // Packages `react` related packages come first.
-              ["^react", "^@?\\w"],
-              // Internal packages.
-              ["^(@|components)(/.*|$)"],
-              // Side effect imports.
-              ["^\\u0000"],
-              // Parent imports. Put `..` last.
-              ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
-              // Other relative imports. Put same-folder imports and `.` last.
-              ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
-              // Style imports.
-              ["^.+\\.?(css)$"],
-            ],
-          },
-        ],
-      },
-    },
   ],
   root: true,
   env: {
